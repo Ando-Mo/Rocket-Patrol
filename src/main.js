2eec6c7579f3a929cfb6d8aee5f438ea451be5d2
Andrew Mangal <amangal@ucsc.edu>
2022-04-21 01:42:49 -0700

    (edited comment)

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 //Name: Andrew Mangal, Project Title: Shooting Fish in a Carnival, Date: 4/21, Length: 10 hours
 
 //Points Breakdown: 
-//60 points for the redesigning of all sound, UI, and art to fit to a specific theme (ie: Circus themed)
+//60 points for the redesigning of all sound, UI, and art to fit to a specific theme (ie: Carnival-booth themed)
 //20 points for the new Pie enemy, which is smaller, worth more points, and moves faster than the rest.
 //10 points for the animated fish enemy (or even the animated clown, if you wanted to consider him an enemy-- there's at least two new loop animated objects)
 //10 points for the timer on the top right hand corner that counts down from 60 seconds.
